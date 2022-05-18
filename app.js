b4817ba41d5b60d3f9d34e2dba0e621103f0d22e
Nadi Cohen <nadi@blings.io>
2022-05-18 16:38:03 +0300

    test app is up

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static("public"))
 
 // define the first route
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+  res.send("<h1>Hello nadi!</h1>")
 })
 
 // start the server listening for requests
